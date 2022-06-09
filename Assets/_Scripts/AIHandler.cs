37dd1687dbfd49e3235f862211e38610a66f8ae2
Tamsin Savery AIE <tamsinsavery@gmail.com>
2022-06-09 18:24:00 +0930

    WHOO finished the turning system, still a couple bugs but we will cross that bridge later.

--- Assets/_Scripts/AIHandler.cs
+++ Assets/_Scripts/AIHandler.cs
@@ -42,10 +42,17 @@ public class AIHandler : MonoBehaviour
         }
 
         inputVector.x = TurnTowardsTarget();
-        inputVector.y = 0.5f;
+        inputVector.y = ApplyThrottleOrBrake(inputVector.x);
     
 
         carMovement.SetInputVector(inputVector);
+    }
+
+    float ApplyThrottleOrBrake(float inputx)
+    {
+        return 0.55f - Mathf.Abs(inputx)/2.0f;
+        //this uses the absolute value of input (so acceleration is always positive) and divides it by 2, so max acceleration is 0.5
+        //subtracts this value from 0.55 so there is always some acceleration 
     }
 
     void FollowWayPoints()
@@ -93,11 +100,11 @@ public class AIHandler : MonoBehaviour
     float TurnTowardsTarget()
     {
         Vector3 vectorToTarget = targetPosition - transform.position;   //gets the angle between the target vector and the transform position
-        //vectorToTarget.Normalize();     //gives the vector a magnitude of 1 instead of a very large number, this gives direction of target
+        vectorToTarget.Normalize();     //gives the vector a magnitude of 1 instead of a very large number, this gives direction of target
 
         float angleToTarget = Vector3.Angle(vectorToTarget, transform.forward); //finds the angle between the target direction and the car's forward direction
 
-        //angleToTarget *= -1;        //inverts the angle, giving the angle the car needs to turn to line up with the target 
+        angleToTarget *= -1;        //inverts the angle, giving the angle the car needs to turn to line up with the target 
 
         float steerAmount = angleToTarget/45;     //This means that due to the clamp in next line, any angle over 45 degrees produces the same steer input as 45 
         steerAmount = Mathf.Clamp(steerAmount, -1.0f, 1.0f);
