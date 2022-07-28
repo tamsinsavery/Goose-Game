2e98841ac0dc64fcb468696bf1f4dc6cbbfa7dbb
Tamsin Savery AIE <tamsinsavery@gmail.com>
2022-07-28 20:28:31 +0930

    Finished character select screen - still need to finalise the level select and join them all together Colliders still need some work Mostly finished end of game state, just need to work out a bit with the UI and join with other systems

---
+++ Assets/_Scripts/CharacterSelect.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public int gooseSelected = 1;
+    public int whichGooseIsThis;
+    public static int finalSelection;
+    public GameObject goose;
+    public Vector3 sizeChange;
+    Vector3 originalSize;
+    Vector3 newSize;
+
+    public Text description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        description.text = "";
+        originalSize = goose.transform.localScale;
+        newSize = goose.transform.localScale+ sizeChange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gooseSelected);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gooseSelected -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gooseSelected+=1;
+        }
+
+        switch (gooseSelected)
+        {
+            case 1:
+                if (whichGooseIsThis == 1)
+                {
+                    goose.transform.localScale = newSize;
+                    description.text = "Regular goose";
+                }
+                else
+                {
+                    goose.transform.localScale  = originalSize;
+                }
+                break;
+            case 2:
+                if (whichGooseIsThis == 2)
+                {
+                    goose.transform.localScale = newSize;
+                    description.text = "The father goose";
+                }
+                else
+                {
+                    goose.transform.localScale = originalSize;
+                }
+                break;
+            case 3:
+                if (whichGooseIsThis == 3)
+                {
+                    goose.transform.localScale = newSize;
+                    description.text = "The son goose";
+                }
+                else
+                {
+                    goose.transform.localScale = originalSize;
+                }
+                break;
+            case 4:
+                if (whichGooseIsThis == 4)
+                {
+                    goose.transform.localScale = newSize;
+                    description.text = "The Holy Ghoost";
+                }
+                else
+                {
+                    goose.transform.localScale = originalSize;
+                }
+                break;
+            case 5:
+                gooseSelected = 1;
+                break;
+            case 0:
+                gooseSelected = 4;
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            finalSelection = gooseSelected;
+            
+        }
+    }
+}
